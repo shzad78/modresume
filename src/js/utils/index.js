356c15efe9c6d9d762916fdf272b46f51711f45e
shahzad babar <shzad78@gmail.com>
2018-07-11 22:47:17 +0500

    utils

---
+++ src/js/utils/index.js
@@ -0,0 +1,12 @@
+// ########## Import Dependencies Here ##########
+import jwtDecode from "jwt-decode";
+
+export function isValidToken() {
+  let token = localStorage.getItem("token");
+  if (token) {
+    let decoded = jwtDecode(token);
+    return decoded.exp > Date.now() / 1000;
+  } else {
+    return false;
+  }
+}
