de13dd61b2c413b1c68643545381f65732b75b53
shahzad babar <shzad78@gmail.com>
2018-07-11 22:45:02 +0500

    api

---
+++ src/js/api/index.js
@@ -0,0 +1,9 @@
+// ########## Import Dependencies Here ##########
+import axios from "axios";
+
+// ########## Import Components Here ##########
+import appData from "../components/appData";
+
+export function loadApp() {
+  return appData;
+}
