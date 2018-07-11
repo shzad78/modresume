079285a7fc41e4d1930cdbf942d7dd68cd9f53c5
shahzad babar <shzad78@gmail.com>
2018-07-11 22:43:24 +0500

    index

---
+++ src/js/index.jsx
@@ -0,0 +1,27 @@
+// ########## Import Dependencies Here ##########
+import ReactDOM from "react-dom";
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import logger from "redux-logger";
+
+// ########## Import Components Here ##########
+import rootReducer from "./reducers";
+import App from "./components/App";
+import rootSaga from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger));
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#app")
+);
+
+export default store;
